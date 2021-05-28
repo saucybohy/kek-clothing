096b7c7cf9ad2d9830e23bba2f9c12b333f4f29a
saucybohy <81395166+saucybohy@users.noreply.github.com>
2021-05-28 11:12:37 +1000

    font size

--- kek-clothing/SqliteDataAccess.cs
+++ kek-clothing/SqliteDataAccess.cs
@@ -13,6 +13,8 @@ namespace kek_clothing
 {
     public class SqliteDataAccess
     {
+       // if the string is not blank it will only load the products that have been selected but if it is left blank all products will load  
+
         public static ObservableCollection<ProductModel> LoadProducts(string category)
         {
             if(category == "")
@@ -20,7 +22,7 @@ namespace kek_clothing
                 using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
                 {
                     var output = cnn.Query<ProductModel>("select * from products", new DynamicParameters());
-                    return new ObservableCollection<ProductModel>(output.ToList());
+                    return new ObservableCollection<ProductModel>(output.ToList()); // creates a list of products, outputs it to the main project
                 }
             }
             else
@@ -32,6 +34,7 @@ namespace kek_clothing
                 }
             }
         }
+      // gathers all the data and adds it to the database 
         public static void AddProduct(string name, string category, float price, string image)
         {
             ProductModel product = new ProductModel();
@@ -41,6 +44,7 @@ namespace kek_clothing
             product.image = image;
             SaveProduct(product);
         }
+       // saves all the data 
         public static void SaveProduct(ProductModel product)
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
@@ -48,6 +52,7 @@ namespace kek_clothing
                 cnn.Execute("insert into products (name, category, price, image) values (@name, @category, @price, @image)", product);
             }
         }
+      // deletes product that its selected
         public static int DeleteProduct(int id)
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
